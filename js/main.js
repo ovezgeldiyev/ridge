f97af10d78855743ecb4bf70dbaba34a05dd8e5e
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2023-09-18 16:19:29 +0500

    asd

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// menu start
+var menu = document.getElementById("menu");
+var menuBtn = document.getElementById("menuBtn");
+var body = document.body;
+menuBtn.onclick = function () {
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  body.classList.toggle("active");
+};
+window.onclick = function (event) {
+  if (event.target == menu) {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("active");
+    body.classList.remove("active");
+  }
+};
+// menu end
